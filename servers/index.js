5e5cae7dd58da1b765aee591ddace761d67e5b1b
Akshat Arora <93427430+akshat2992001@users.noreply.github.com>
2023-01-23 18:34:08 +0530

    First Commit
    Completed Setup of Server and added Babel to it , Made Mongo Connection.

---
+++ servers/index.js
@@ -0,0 +1,36 @@
+import  express  from "express";
+import dotenv from "dotenv";
+
+//Database Connection
+
+import ConnectDB from "./database/connection";
+
+dotenv.config();
+
+const zomato = express();
+
+zomato.use(express.json());
+
+zomato.get("/", (request,response) => {
+    response.json({
+        message: "Server is running",
+    });
+});
+
+const PORT = 4000;
+
+zomato.listen(PORT, () => {
+//  ConnectDB()
+//     .then(() => {
+//     console.log("Server is running !!!");
+//     })
+
+//     .catch((error) => {
+//         console.log("Server is running but database connection failed...");
+//         console.log("Error");
+
+//     });
+
+    console.log("Server is running !!!");
+ 
+});
